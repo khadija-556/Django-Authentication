a6e475e848341b95bad44f21ce685ca59689b65c
khadija-556 <khatukhadija556@gmail.com>
2026-08-11 23:24:40 +0600

    Create CustomUser model

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.EmailField(unique = True, blank=True, null=True)
+    phone = models.CharField (max_length = 25, unique = True, blank=True, null=True)
+    STATUS_CHOICES = (
+        ('active','Active'),
+        ('inactive','Inactive'),
+        ('blocked','Blocked'),
+    )
+    user_status = models.CharField(max_length = 20,choices = STATUS_CHOICES, default='active')
+    photo_url = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.authentication',
 ]
 
@@ -126,3 +127,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
